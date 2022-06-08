ead486fc07a0c4d6bcc6d4a29190479f6613077a
daking550 <david.king550wil@gmail.com>
2022-06-08 17:30:53 -0400

    Register Userfixed

--- ElevenNote.WebAPI/Controllers/UserController.cs
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -5,16 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ElevenNote.WebAPI.Controllers
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
+    private readonly IUserService _service;
+    public UserController(IUserService service)
     {
-        private readonly IUserService _service;
-        public UserController(IUserService service)
+        _service = service;
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
+    {
+        if (!ModelState.IsValid)
         {
-            _service =  service;
+            return BadRequest(ModelState);
         }
+
+        var registerResult = await _service.RegisterUserAsync(model);
+
+        if (registerResult)
+        {
+            return Ok("User was registered.");
+        }
+
+        return BadRequest("User could not be registered.");
     }
 }
+
